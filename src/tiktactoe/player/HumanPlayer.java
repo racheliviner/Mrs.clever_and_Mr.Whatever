a8b36c2efe6de99cc897e69c943c8d6bbeba5ca7
Racheli Viner <150911456+racheliviner@users.noreply.github.com>
2024-01-06 20:06:00 +0200

    Update HumanPlayer.java

--- src/tiktactoe/player/HumanPlayer.java
+++ src/tiktactoe/player/HumanPlayer.java
@@ -5,20 +5,43 @@ import tiktactoe.main.Mark;
 
 import java.util.Scanner;
 
+/**
+ * The HumanPlayer class represents a human player in Tic-Tac-Toe.
+ */
 public class HumanPlayer implements Player {
-    //    void playTurn (tiktactoe.main.Board board, tiktactoe.main.Mark mark)
     private Scanner scanner = new Scanner(System.in);
-    private String name = "Human player";
+    private String name = "Human Player";
 
+    /**
+     * Plays a turn for the HumanPlayer by accepting user input for the move coordinates.
+     *
+     * @param board The game board.
+     * @param mark  The player's mark.
+     */
     public void playTurn(Board board, Mark mark) {
         while (true) {
-            System.out.format("Choose coordinates for %s: ", mark);
-            int cord = scanner.nextInt();
-            if (board.putMark(mark, cord / 10 - 1, cord % 10 - 1))
+            System.out.format("Choose coordinates for %s (row, col): ", mark);
+            int coordinates = scanner.nextInt();
+
+            // Extract row and column from the user input
+            int row = coordinates / 10 - 1;
+            int col = coordinates % 10 - 1;
+
+            // Attempt to place the mark on the board
+            if (board.putMark(mark, row, col)) {
                 return;
+            } else {
+                System.out.println("Invalid move. Try again.");
+            }
         }
     }
-    public String getName(){
+
+    /**
+     * Gets the name of the HumanPlayer.
+     *
+     * @return The player's name.
+     */
+    public String getName() {
         return this.name;
     }
 }
