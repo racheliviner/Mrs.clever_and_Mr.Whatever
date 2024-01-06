3a80fb26339d97e73efd0b3f0c15525ee70c0e24
Racheli Viner <150911456+racheliviner@users.noreply.github.com>
2024-01-06 20:07:38 +0200

    Update WhateverPlayer.java

--- src/tiktactoe/player/WhateverPlayer.java
+++ src/tiktactoe/player/WhateverPlayer.java
@@ -3,22 +3,39 @@ package tiktactoe.player;
 import tiktactoe.main.Board;
 import tiktactoe.main.Mark;
 
-import java.util.*;
+import java.util.Random;
 
+/**
+ * The WhateverPlayer class represents a player with a random move strategy in Tic-Tac-Toe.
+ */
 public class WhateverPlayer implements Player {
-    private Scanner scanner = new Scanner(System.in);
     private Random rand = new Random();
     private String name = "Mr. Whatever";
 
+    /**
+     * Plays a turn for the WhateverPlayer by making random moves on the game board.
+     *
+     * @param board The game board.
+     * @param mark  The player's mark.
+     */
     public void playTurn(Board board, Mark mark) {
         while (true) {
             int row = rand.nextInt(board.SIZE);
             int col = rand.nextInt(board.SIZE);
-            if (board.putMark(mark, row, col))
+
+            // Attempt to place the mark randomly on the board
+            if (board.putMark(mark, row, col)) {
                 return;
+            }
         }
     }
-    public String getName(){
+
+    /**
+     * Gets the name of the WhateverPlayer.
+     *
+     * @return The player's name.
+     */
+    public String getName() {
         return this.name;
     }
 }
